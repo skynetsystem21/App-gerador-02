14c744089169b57f0358e162c4d3ae565c22e4ce
skynetsystem21 <skynetsystem21@gmail.com>
2021-03-13 12:14:53 -0300

    finished app

--- App.js
+++ App.js
@@ -1,10 +1,101 @@
-import React from 'react';
-import {  View, Text } from 'react-native';
+import React, { useState } from 'react';
+import {  View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+import Slider from '@react-native-community/slider';
+import Clipboard from 'expo-clipboard';
+
+let charset = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789';
 
 export default function App() {
+  const [password, setPassword] = useState('');
+  const [size, setSize] = useState(5);
+
+  function generatePass() {
+    let pass = '';
+    for(let i = 0, n = charset.length; i < size; i++) {
+      pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+    setPassword(pass);
+  }
+
+  function copyPass() {
+    Clipboard.setString(password);
+    alert('Senha copiada com sucesso!');
+  }
   return(
-     <View style={{ marginTop: 60 }}>
-       <Text>Aprendendo React Native</Text>
+     <View style = {styles.container}>
+       <Image 
+       source={require('./src/assets/logo.png')}
+       style={styles.logo}
+       />
+       <Text style={styles.title}>{size} Caracteres</Text>
+
+       <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor="#FF0000"
+          maximumTrackTintColor="#000"
+          value={size}
+          onValueChange={ (valor) => setSize(valor.toFixed(0)) }
+        />
+       </View>
+    
+    <TouchableOpacity style={styles.button} onPress={generatePass} >
+      <Text style={styles.buttonText}>Gerar Senha</Text>
+    </TouchableOpacity>
+
+    {password !== '' && (
+       <View style={styles.area}>
+       <Text style={styles.password} onLongPress={copyPass}> {password} </Text>
+      </View>
+    )}
+   
+
      </View>
   )
+}   
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: '#F3F3FF'
+},
+logo: {
+  marginBottom: 60
+},
+title: {
+  fontSize: 20,
+  fontWeight: 'bold'
+},
+area: {
+  marginTop: 15,
+  marginBottom: 15,
+  backgroundColor: '#FFF',
+  width: '80%',
+  borderRadius: 10
+},
+button: {
+  backgroundColor: '#FFA200',
+  width: '80%',
+  height: 50,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 10,
+  marginBottom: 25
+},
+buttonText: {
+  fontSize: 20,
+  color: '#FFF',
+  fontWeight: 'bold'
+},
+password: {
+  padding: 10,
+  textAlign: 'center',
+  fontSize: 20
 }
+
+});
